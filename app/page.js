c0aecbcaf8c1bcbb3d555bbd48c0407765e3185b
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-09-24 22:23:22 +0600

    get updated code

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/footer/Footer";
 import Newsletter from "./components/newsletter/Newsletter";
 import PopularProduct from "./components/home/PopularProduct";
-import Banner from "./components/home/Banner";
+// import Banner from "./components/home/Banner";
 import SpecialOffers from "./components/home/SpecialOffers";
+import Banner from "./components/home/banner/Banner";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
      <Newsletter></Newsletter>
      <PopularProduct/>
      <SpecialOffers/>
+     {/* <Banner></Banner> */}
      <Banner></Banner>
      <Footer></Footer>
     </main>
