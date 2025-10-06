0b9f94da2124c3200f3f2ac4897f68298619aef9
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-10-07 02:36:49 +0600

    Updated Dashboard Navbar and Fixing error

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,17 +1,57 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useRef, useState } from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import DashboardNavbar from '../components/dashNav/DashNav'
+import { usePathname } from 'next/navigation';
 
 export default function DashboardLayout({ children }) {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const sidebarRef = useRef(null);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+    useEffect(() => {
+    toggleSidebar();
+  }, [pathname]);
+
+    useEffect(() => {
+    function handleClickOutsideEvent(event) {
+      if (sidebarRef.current && !sidebarRef?.current?.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutsideEvent);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideEvent);
+    };
+  }, []);
+
+
   return (
-      <main className="flex">
-        <div className='hidden md:block'><Sidebar /></div>
-        <div className='flex-1 flex flex-col'>
-          <DashboardNavbar></DashboardNavbar>
-          <section className="flex-1">
-            {children}
-          </section>
-        </div>
-      </main>
+    <main className="relative flex">
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+      <div
+        ref={sidebarRef}
+        className={`fixed md:hidden ease-in-out transition-all duration-300 z-50 
+        ${isOpen ? "translate-x-0" : "-translate-x-[260px]"}
+        `}
+      >
+        <Sidebar />
+      </div>
+
+
+      <section className="flex-1 flex flex-col min-h-screen overflow-hidden">
+        <DashboardNavbar toggleSidebar={toggleSidebar}/>
+        <section className="pt-14 flex-1 bg-[#eff3f4]">{children}</section>
+      </section>
+    </main>
   )
 }
