2f424cc57bc45ee0973ed9f97f722f272d6d2b2c
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-10-06 23:46:21 +0600

    Packege-lock.json File error sloved

--- app/components/dashNav/DashNav.jsx
+++ app/components/dashNav/DashNav.jsx
@@ -6,6 +6,8 @@ import { Menu } from "lucide-react";
 export default function DashboardNavbar({ toggleSidebar }) {
   const { user } = useAuth();
   // const { data: admin } = useAdmin({ email: user?.email });
+
+  
   return (
     <section className="fixed w-full top-0 flex items-center gap-3 bg-white border-b px-4 py-3">
       <div className="flex justify-center items-center md:hidden">
