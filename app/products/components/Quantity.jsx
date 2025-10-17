738d882f56c77a9833b386f15bba85ff169a9520
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-10-17 16:44:02 +0600

    Added Functionality on Add to Cart button

--- app/products/components/Quantity.jsx
+++ app/products/components/Quantity.jsx
@@ -4,15 +4,41 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast, ToastContainer } from "react-toastify";
 import Link from "next/link";
+import axios from "axios";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Quantity({ product }) {
+
+  const { user } = useAuth();
+
+
   const [quantity, setQuantity] = useState(1);
   const router = useRouter();
 
-  const handleAddToCart = () => {
-    toast.success(`${product.name} added to cart!`);
-    setTimeout(() => router.push("/cart"), 2000);
-  };
+  // const handleAddToCart = () => {
+  //   toast.success(`${product.name} added to cart!`);
+  //   setTimeout(() => router.push("/cart"), 2000);
+  // };
+
+    const handleAddToCart = async (product) => {
+      const cartItem = {
+        userEmail: user?.email,
+        productId: product._id,
+        name: product.name,
+        price: product.price,
+        image: product.image,
+        quantity: 1,
+      };
+  
+      axios.post("https://smart-shop-server-three.vercel.app/addToCart", cartItem)
+        .then(res => {
+          if (res.data?.insertedId) {
+            toast.success("Added to cart");
+          }
+        })
+        .catch(err => console.log(err));
+      console.log(cartItem);
+    };
 
   return (
     <div className="flex items-center gap-6 mt-4">
@@ -35,7 +61,7 @@ export default function Quantity({ product }) {
       {/* Buttons */}
       <div className="flex  sm:flex-row justify-center lg:justify-start gap-3 w-full lg:w-auto">
         <button
-          onClick={handleAddToCart}
+          onClick={() => handleAddToCart(product)}
           className="px-8 py-3 bg-gray-900 text-white rounded-lg font-medium hover:bg-gray-700 transition-all hover:cursor-pointer"
         >
           Add to Cart
