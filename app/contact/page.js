ab3398224791dfcffa7555841ca9b00da9cd24a4
sharmin-khan <sharminkhancse@gmail.com>
2025-10-28 18:49:58 +0600

    Update UI desing in contact page and add motion for animation

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,5 +1,4 @@
 
-"use client";
 import React from "react";
 import ContactForm from "./components/ContactForm";
 import ContactInfo from "./components/ContactInfo";
@@ -15,19 +14,32 @@ export default function ContactPage() {
   return (
     <>
     <Navbar/>
-    <div className="max-w-5xl mx-auto px-4 py-12">
+    <div className="max-w-7xl mx-auto px-4 py-12">
       <h1 className="text-4xl font-bold text-center mb-4">Get in Touch</h1>
       <p className="text-center mb-10">
         We’d love to hear from you! Fill out the form below or reach us through any of the methods listed.
       </p>
 
-      <div className="grid md:grid-cols-2 gap-10">
+      {/* <div className="grid md:grid-cols-2 gap-10">
         <ContactForm />
         <div className="space-y-6">
           <ContactInfo />
           <GoogleMap />
         </div>
-      </div>
+      </div> */}
+     <div className="grid md:grid-cols-2 gap-10 items-start">
+  {/* Left side - Form */}
+  <div className="w-full">
+    <ContactForm />
+  </div>
+
+  {/* Right side - Info + Map */}
+  <div className="flex flex-col justify-between gap-6 w-full">
+    <ContactInfo />
+    <GoogleMap />
+  </div>
+</div>
+
 
       <SocialLinks />
       <FAQSection />
